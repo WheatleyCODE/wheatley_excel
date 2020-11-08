33784d10e5e51303bd7cd44887a2c663be2caab2
WheatleyCODE <WheatleyCODE@users.noreply.github.com>
2020-11-08 16:54:17 +0300

    test

--- src/redux/constants.js
+++
@@ -1,8 +0,0 @@
-export const COLS_RESIZE = 'COLS_RESIZE'
-export const ROWS_RESIZE = 'ROWS_RESIZE'
-export const CHANGE_TEXT = 'CHANGE_TEXT'
-
-export const APPLY_STYLE = 'APPLY_STYLE'
-export const CHANGE_STYLES = 'CURENT_STYLE'
-export const CHANGE_TABLE_NAME = 'CHANGE_TABLE_NAME'
-
